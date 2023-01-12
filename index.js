983474d632a9c61d0c51114913abfa8cbd696099
Hara <harachung90@gmail.com>
2023-01-12 20:29:45 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let count = 0
+
+let countEL = document.getElementById("count-el")
+let saveEL = document.getElementById("save-el")
+
+function increment() {
+    count += 1;
+    countEL.textContent = count
+}
+
+function save() {
+    let countStr = count + " + "
+    saveEL.textContent += countStr
+    count = 0;
+    countEL.textContent = count
+}
+
+
+
+
